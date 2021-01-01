948adb63bd004e62a7a5e46dc72504e9c48228ad
SUZUKI Masaya <suzukimasaya428@gmail.com>
2021-01-01 16:55:48 +0900

    start.shに与えた引数をgcloudコマンドに与えるようにする

--- gcp/datastore/start.sh
+++ gcp/datastore/start.sh
@@ -5,4 +5,4 @@ gcloud config set project "${DATASTORE_PROJECT_ID}"
 gcloud beta emulators datastore start \
   --data-dir=/opt/data \
   --host-port="${DATASTORE_LISTEN_ADDRESS}" \
-  "${options}"
+  "${@}"
