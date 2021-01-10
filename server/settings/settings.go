6b3324b6d7c6ed64d768e1d9fe5c9b0d4a8fa006
massongit <suzukimasaya428@gmail.com>
2021-01-10 18:42:05 +0900

    Fix

--- server/settings/settings.go
+++ server/settings/settings.go
@@ -1,23 +1,23 @@
 package settings
 
-type SettingsType struct {
+type Type struct {
 	Count          int64
 	ShortURLLength ShortURLLengthSettingsType
 }
 
 type ShortURLLengthSettingsType struct {
 	Max int
-	Min MinShortURLLengthSettingsTyoe
+	Min MinShortURLLengthSettingsType
 }
 
-type MinShortURLLengthSettingsTyoe struct {
+type MinShortURLLengthSettingsType struct {
 	Default int
 	Long    int
 	Short   int
 }
 
 var (
-	minShortURLLengthSettings = MinShortURLLengthSettingsTyoe{Default: 8, Long: 40, Short: 5}
+	minShortURLLengthSettings = MinShortURLLengthSettingsType{Default: 8, Long: 40, Short: 5}
 	shortURLLengthSettings    = ShortURLLengthSettingsType{Max: 64, Min: minShortURLLengthSettings}
-	Settings                  = SettingsType{Count: int64(3), ShortURLLength: shortURLLengthSettings}
+	Settings                  = Type{Count: int64(3), ShortURLLength: shortURLLengthSettings}
 )
