8fa6e4b381f6b290a1b1abe9fa1d59af308df0e1
masaya.suzuki <suzukimasaya428@gmail.com>
2022-05-02 19:20:49 +0900

    パス修正

--- elm/make_compiler.sh
+++ elm/make_compiler.sh
@@ -4,6 +4,6 @@ cd ..
 image_tag='hato_atama_elm_compiler'
 docker build --platform=linux/arm64 -t "${image_tag}" -f elm/Dockerfile .
 container_id=$(docker create "${image_tag}")
-docker cp "${container_id}:/usr/local/bin/elm" elm_arm64
+docker cp "${container_id}:/usr/local/bin/elm" elm/elm_arm64
 docker stop "${container_id}"
 docker rm "${container_id}"
