4e1525462496be3c573f5d9bef23b0e406af1ca1
masaya.suzuki <suzukimasaya428@gmail.com>
2022-05-02 19:19:25 +0900

    platformを明示的に指定

--- elm/make_compiler.sh
+++ elm/make_compiler.sh
@@ -2,7 +2,7 @@
 
 cd ..
 image_tag='hato_atama_elm_compiler'
-docker build -t "${image_tag}" -f elm/Dockerfile .
+docker build --platform=linux/arm64 -t "${image_tag}" -f elm/Dockerfile .
 container_id=$(docker create "${image_tag}")
 docker cp "${container_id}:/usr/local/bin/elm" elm_arm64
 docker stop "${container_id}"
