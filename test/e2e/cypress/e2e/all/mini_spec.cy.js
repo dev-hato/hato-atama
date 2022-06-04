7b33e8d7ba95756edee8928c686a4666655a07da
Masaya Suzuki <15100604+massongit@users.noreply.github.com>
2022-06-04 13:24:42 +0900

    super-linterでE2Eテストのファイルも見るようにする (#1040)
    * super-linterでE2Eテストのファイルも見る
    
    * 鳩は唐揚げ！(自動で直してあげたよ！) (#1041)
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Cypress関連の変数を明示的にimportする
    
    * globalコメントに移動
    
    * globalコメントに移動
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- test/e2e/cypress/e2e/all/mini_spec.cy.js
+++ test/e2e/cypress/e2e/all/mini_spec.cy.js
@@ -1,30 +1,32 @@
+/* global Cypress, cy, describe, it, expect */
+
 describe('短縮URLを生成できる', () => {
   it('三回までは200だが、4回目以降は404が返る', () => {
-    const apiHost = Cypress.env('API_HOST');
+    const apiHost = Cypress.env('API_HOST')
 
-    cy.visit(apiHost);
+    cy.visit(apiHost)
 
-    cy.get('#raw_url').type(apiHost);
+    cy.get('#raw_url').type(apiHost)
 
-    cy.get('button').click();
+    cy.get('button').click()
 
     cy.get('a')
       .should('have.attr', 'href')
       .then(($url) => {
         cy.request({ method: 'GET', url: $url }).then((response) => {
-          expect(response.status).to.eq(200);
-        });
+          expect(response.status).to.eq(200)
+        })
         cy.request({ method: 'GET', url: $url }).then((response) => {
-          expect(response.status).to.eq(200);
-        });
+          expect(response.status).to.eq(200)
+        })
         cy.request({ method: 'GET', url: $url }).then((response) => {
-          expect(response.status).to.eq(200);
-        });
+          expect(response.status).to.eq(200)
+        })
         cy.request({ method: 'GET', url: $url, failOnStatusCode: false }).then(
           (response) => {
-            expect(response.status).to.eq(404);
+            expect(response.status).to.eq(404)
           }
-        );
-      });
-  });
-});
+        )
+      })
+  })
+})
