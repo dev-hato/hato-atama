bb92b6f8f25b39a1768a68f8408aadaecdebd157
masaya.suzuki <suzukimasaya428@gmail.com>
2022-11-03 01:48:12 +0900

    super-linterによる指摘事項修正

--- scripts/sleep.js
+++ scripts/sleep.js
@@ -1,12 +1,12 @@
-module.exports = async ({result, running, retry_count}) => {
-    if (!result.includes(running) || i === retry_count - 1) {
-        return
-    }
+module.exports = async ({ result, running, retryCount, i }) => {
+  if (!result.includes(running) || i === retryCount - 1) {
+    return
+  }
 
-    // 完了していないrunがあった場合はリトライ
-    // sleepする時間は exponential backoff and jitter で算出している
-    // 参考: https://aws.typepad.com/sajp/2015/03/backoff.html
-    const sleep_seconds = Math.random() * (Math.pow(2, i + 1) * 100)
-    console.log(`sleep ${sleep_seconds}s`)
-    await new Promise(r => setTimeout(r, sleep_seconds * 1000))
+  // 完了していないrunがあった場合はリトライ
+  // sleepする時間は exponential backoff and jitter で算出している
+  // 参考: https://aws.typepad.com/sajp/2015/03/backoff.html
+  const sleepSeconds = Math.random() * (Math.pow(2, i + 1) * 100)
+  console.log(`sleep ${sleepSeconds}s`)
+  await new Promise(resolve => setTimeout(resolve, sleepSeconds * 1000))
 }
