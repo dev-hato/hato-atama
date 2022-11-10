1022d8263e716fedd174e478ddd543c9f0a8d7a9
masaya.suzuki <suzukimasaya428@gmail.com>
2022-11-11 08:19:38 +0900

    スクリプト修正

--- scripts/release/run_docker_compose.sh
+++ scripts/release/run_docker_compose.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-cat .env >>"$GITHUB_ENV"
 docker compose -f docker-compose.yml -f staging.docker-compose.yml pull
 docker compose -f docker-compose.yml -f staging.docker-compose.yml up -d --wait
