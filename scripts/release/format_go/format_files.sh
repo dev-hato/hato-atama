ad09ea9c3850bc7cea80a27a2b841db5c4e92f8f
masaya.suzuki <suzukimasaya428@gmail.com>
2022-11-10 20:57:03 +0900

    grepの代わりにyqを使う

--- scripts/release/format_go/format_files.sh
+++ scripts/release/format_go/format_files.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-go_version=$(grep runtime app.yaml | sed -e 's/runtime: go\([0-9]\)\([0-9]*\)/\1.\2/g')
+go_version=$(yq '.runtime' app.yaml | sed -e 's/go\([0-9]\)\([0-9]*\)/\1.\2/g')
 go mod tidy -go="${go_version}"
 goimports -l -w .
--- scripts/update_go_runtime/update_go_runtime/npm_ci.sh
+++ scripts/update_go_runtime/update_go_runtime/npm_ci.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-npm_version=$(grep \"npm\" package.json | sed -e 's/ *"npm": "^\(.*\)"/\1/g')
+npm_version=$(yq '.engines.npm' package.json | sed -e 's/\^//g')
 npm install --location=global "npm@${npm_version}"
 npm ci
