d75f416e8c6c8cdc08951e9d73bffd6a2e725219
masaya.suzuki <suzukimasaya428@gmail.com>
2022-11-11 07:29:54 +0900

    スクリプト修正

--- scripts/release/e2e_test_all_docker_compose/set_env.sh
+++ scripts/release/e2e_test_all_docker_compose/set_env.sh
@@ -2,4 +2,3 @@
 
 ENV="API_HOST=http://localhost:${FRONTEND_PORT}/"
 echo "ENV=${ENV}" >>"${GITHUB_ENV}"
-bash "${GITHUB_WORKSPACE}/scripts/release/run_test.sh"
--- scripts/release/e2e_test_all_prd/set_env.sh
+++ scripts/release/e2e_test_all_prd/set_env.sh
@@ -3,4 +3,3 @@
 ENV="API_HOST="
 ENV+="https://v${GITHUB_RUN_NUMBER}-dot-hato-atama.an.r.appspot.com"
 echo "ENV=${ENV}" >>"${GITHUB_ENV}"
-bash "${GITHUB_WORKSPACE}/scripts/release/run_test.sh"
