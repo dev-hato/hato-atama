a5a99a53c38f92955547df2c92119acef9e973a0
masaya.suzuki <suzukimasaya428@gmail.com>
2022-11-22 08:30:52 +0900

    PR名を取得できるようにする・escape処理追加

--- scripts/fail_notify/fail_notify/get_slack_payload.js
+++ scripts/fail_notify/fail_notify/get_slack_payload.js
@@ -1,3 +1,11 @@
+function escape (s) {
+  for (const replaceRule of [['&', '&amp;'], ['<', '&lt;'], ['>', '&gt;']]) {
+    s = s.replaceAll(replaceRule[0], replaceRule[1])
+  }
+
+  return s
+}
+
 module.exports = ({ context }) => {
   console.log(context.payload)
   const payload = {
@@ -13,12 +21,12 @@ module.exports = ({ context }) => {
   if (context.payload.workflow_run.event === 'pull_request') {
     payload.attachments.unshift({
       title: 'PR',
-      value: context.payload.workflow_run.pull_requests.map(p => p.number).map(n => `<${n}|${context.payload.workflow_run.head_repository.html_url}/pulls/${n}>`).join('\n')
+      value: context.payload.workflow_run.pull_requests.map(p => `<${escape(context.payload.workflow_run.display_title)}|${context.payload.workflow_run.head_repository.html_url}/pulls/${p.number}>`).join('\n')
     })
   } else if (context.payload.workflow_run.head_commit && context.payload.workflow_run.head_repository) {
     payload.attachments.unshift({
       title: 'コミット',
-      value: `<${context.payload.workflow_run.head_commit.message}|${context.payload.workflow_run.head_repository.html_url}/commit/${context.payload.workflow_run.head_commit.id}>`
+      value: `<${escape(context.payload.workflow_run.head_commit.message)}|${context.payload.workflow_run.head_repository.html_url}/commit/${context.payload.workflow_run.head_commit.id}>`
     })
   }
 
