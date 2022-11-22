33c9f0debf9212e739f2d0436c64d0520f18ca16
Masaya Suzuki <15100604+massongit@users.noreply.github.com>
2022-11-22 22:16:42 +0900

    コミット時の表示修正

--- scripts/fail_notify/fail_notify/get_slack_payload.js
+++ scripts/fail_notify/fail_notify/get_slack_payload.js
@@ -1,12 +1,3 @@
-// https://api.slack.com/reference/surfaces/formatting#escaping
-function escape (s) {
-  for (const replaceRule of [['&', '&amp;'], ['<', '&lt;'], ['>', '&gt;']]) {
-    s = s.replaceAll(replaceRule[0], replaceRule[1])
-  }
-
-  return s
-}
-
 module.exports = ({ context }) => {
   console.log(context.payload)
   const payload = {
@@ -18,16 +9,22 @@ module.exports = ({ context }) => {
       }
     ]
   }
+  let displayTitle = context.payload.workflow_run.display_title
+
+  // https://api.slack.com/reference/surfaces/formatting#escaping
+  for (const replaceRule of [['&', '&amp;'], ['<', '&lt;'], ['>', '&gt;']]) {
+    displayTitle = displayTitle.replaceAll(replaceRule[0], replaceRule[1])
+  }
 
   if (context.payload.workflow_run.event === 'pull_request') {
     payload.attachments.unshift({
       title: 'PR',
-      value: context.payload.workflow_run.pull_requests.map(p => `<${escape(context.payload.workflow_run.display_title)}|${context.payload.workflow_run.head_repository.html_url}/pulls/${p.number}>`).join('\n')
+      value: context.payload.workflow_run.pull_requests.map(p => `<${displayTitle}|${context.payload.workflow_run.head_repository.html_url}/pulls/${p.number}>`).join('\n')
     })
   } else if (context.payload.workflow_run.head_commit && context.payload.workflow_run.head_repository) {
     payload.attachments.unshift({
       title: 'コミット',
-      value: `<${escape(context.payload.workflow_run.head_commit.message)}|${context.payload.workflow_run.head_repository.html_url}/commit/${context.payload.workflow_run.head_commit.id}>`
+      value: `<${displayTitle}|${context.payload.workflow_run.head_repository.html_url}/commit/${context.payload.workflow_run.head_commit.id}>`
     })
   }
 
