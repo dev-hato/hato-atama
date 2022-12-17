7b70e01c0c6d2f64f7a191f7cbe032e7eafc152b
masaya.suzuki <suzukimasaya428@gmail.com>
2022-12-17 22:25:01 +0900

    super-linterによる指摘事項修正

--- scripts/block-merge-pr/block-merge-pr/check-labels.js
+++ scripts/block-merge-pr/block-merge-pr/check-labels.js
@@ -1,7 +1,7 @@
-module.exports = ({context}) => {
-    for (const label of context.payload.pull_request.labels) {
-        if (label.name === 'do not merge') {
-            throw new Error('「do not merge」ラベルが付与されているためマージできません。');
-        }
+module.exports = ({ context }) => {
+  for (const label of context.payload.pull_request.labels) {
+    if (label.name === 'do not merge') {
+      throw new Error('「do not merge」ラベルが付与されているためマージできません。')
     }
+  }
 }
