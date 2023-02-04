93dbeaddca2d5c224211be650a66b368a67801eb
masaya.suzuki <suzukimasaya428@gmail.com>
2023-02-04 10:59:45 +0900

    docker compose v2対応

--- scripts/get_go_version.sh
+++ scripts/get_go_version.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-docker compose -f docker-compose.yml -f dev.docker-compose.yml pull
+docker compose -f compose.yml -f dev.compose.yml pull
 DOCKER_CMD="go version | awk '{print \$3}' | sed -e 's/^go//g'"
-go_version=$(docker compose -f docker-compose.yml -f dev.docker-compose.yml run server sh -c "${DOCKER_CMD}")
+go_version=$(docker compose -f compose.yml -f dev.compose.yml run server sh -c "${DOCKER_CMD}")
 echo "Go version:" "${go_version}"
 echo "go_version=${go_version}" >>"${GITHUB_OUTPUT}"
--- scripts/release/update_version/get_node_version.sh
+++ scripts/release/update_version/get_node_version.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-docker compose -f docker-compose.yml -f base.docker-compose.yml pull frontend
-mapfile -t result < <(docker compose -f docker-compose.yml -f base.docker-compose.yml run frontend sh -c "${DOCKER_CMD}")
+docker compose -f compose.yml -f base.compose.yml pull frontend
+mapfile -t result < <(docker compose -f compose.yml -f base.compose.yml run frontend sh -c "${DOCKER_CMD}")
 node_version="${result[0]//v/}"
 npm_version=${result[1]}
 echo "Node.js version:" "${node_version}"
