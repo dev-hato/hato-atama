b62bdefc1276ccba0bc841f9413fd79ef8ed6e94
Masaya Suzuki <suzukimasaya428@gmail.com>
2023-02-28 00:25:57 +0900

    Dockerfile修正

--- scripts/release/dockle/run_dockle.sh
+++ scripts/release/dockle/run_dockle.sh
@@ -12,9 +12,15 @@ for image_name in $(docker compose -f compose.yml -f "${DOCKER_COMPOSE_FILE_NAME
 
   if [[ "${image_name}" =~ "gcloud_datastore" ]] || [[ "${image_name}" =~ "server-dev" ]] || [[ "${image_name}" =~ "server-base" ]]; then
     cmd+="-i DKL-LI-0003 "
-    if [[ "${image_name}" =~ "gcloud_datastore" ]]; then
-      cmd+="-af settings.py -i CIS-DI-0001 "
-    else
+
+    if [[ "${image_name}" =~ "gcloud_datastore" ]] || [[ "${image_name}" =~ "server-base" ]]; then
+      cmd+="-i CIS-DI-0001 "
+      if [[ "${image_name}" =~ "gcloud_datastore" ]]; then
+        cmd+="-af settings.py "
+      fi
+    fi
+
+    if [[ "${image_name}" =~ "server-dev" ]] || [[ "${image_name}" =~ "server-base" ]]; then
       cmd+="--timeout 600s "
       if [[ "${image_name}" =~ "server-dev" ]]; then
         cmd+="-af credentials "
