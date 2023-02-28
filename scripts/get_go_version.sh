125a7f2bdfe8eb716f55d4c1e0ebeed21c95a8fb
Masaya Suzuki <15100604+massongit@users.noreply.github.com>
2023-02-28 19:57:20 +0900

    github.com/cosmtrek/airアップデート (#2613)
    * Update module github.com/cosmtrek/air to v1.42.0
    
    * serverのベースイメージ追加
    
    * formatが間違ってたので直してあげたよ！ (#2614)
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * depends_on移動
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- scripts/get_go_version.sh
+++ scripts/get_go_version.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-docker compose -f compose.yml -f dev.compose.yml pull
+docker compose -f compose.yml -f base.compose.yml pull server
 DOCKER_CMD="go version | awk '{print \$3}' | sed -e 's/^go//g'"
-go_version=$(docker compose -f compose.yml -f dev.compose.yml run server sh -c "${DOCKER_CMD}")
+go_version=$(docker compose -f compose.yml -f base.compose.yml run server sh -c "${DOCKER_CMD}")
 echo "Go version:" "${go_version}"
 echo "go_version=${go_version}" >>"${GITHUB_OUTPUT}"
