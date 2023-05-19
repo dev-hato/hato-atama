456bc44e87c1763d9014b30c515d09d988f86d6e
Masaya Suzuki <suzukimasaya428@gmail.com>
2023-05-20 08:43:02 +0900

    update-versionでnpm installも行う

--- scripts/release/update_version/update_versions.sh
+++ scripts/release/update_version/update_versions.sh
@@ -1,23 +1,22 @@
 #!/usr/bin/env bash
 
-for path in "frontend" "test/e2e" "."; do
-  echo "${NODE_VERSION}" >${path}/.node-version
+echo "${NODE_VERSION}" >.node-version
 
-  NODE_PATTERN="s/\"node\": \".*\"/\"node\": \"^${DEPENDABOT_NODE_VERSION}"
+NODE_PATTERN="s/\"node\": \".*\"/\"node\": \"^${DEPENDABOT_NODE_VERSION}"
 
-  if [ "${DEPENDABOT_NODE_VERSION}" != "${NODE_VERSION}" ]; then
-    NODE_PATTERN+=" || ^${NODE_VERSION}"
-  fi
+if [ "${DEPENDABOT_NODE_VERSION}" != "${NODE_VERSION}" ]; then
+  NODE_PATTERN+=" || ^${NODE_VERSION}"
+fi
 
-  NODE_PATTERN+="\"/g"
-  sed -i -e "${NODE_PATTERN}" ${path}/package.json
+NODE_PATTERN+="\"/g"
+sed -i -e "${NODE_PATTERN}" package.json
 
-  NPM_PATTERN="s/\"npm\": \".*\"/\"npm\": \"^${DEPENDABOT_NPM_VERSION}"
+NPM_PATTERN="s/\"npm\": \".*\"/\"npm\": \"^${DEPENDABOT_NPM_VERSION}"
 
-  if [ "${DEPENDABOT_NPM_VERSION}" != "${NPM_VERSION}" ]; then
-    NPM_PATTERN+=" || ^${NPM_VERSION}"
-  fi
+if [ "${DEPENDABOT_NPM_VERSION}" != "${NPM_VERSION}" ]; then
+  NPM_PATTERN+=" || ^${NPM_VERSION}"
+fi
 
-  NPM_PATTERN+="\"/g"
-  sed -i -e "${NPM_PATTERN}" ${path}/package.json
-done
+NPM_PATTERN+="\"/g"
+sed -i -e "${NPM_PATTERN}" package.json
+npm install
