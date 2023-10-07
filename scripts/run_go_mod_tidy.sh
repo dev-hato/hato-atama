6cda0f9ceb12b6f02622011b100dd6cf5187f74c
massongit <suzukimasaya428@gmail.com>
2023-10-07 14:56:21 +0900

    Revert "app.yamlからのGoバージョン取得処理修正"
    This reverts commit 69351257d0f6c9b15d6408903af95fef29d01be6.

--- scripts/run_go_mod_tidy.sh
+++ scripts/run_go_mod_tidy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-go_version=$(yq '.runtime_config.runtime_version' app.yaml)
+go_version=$(yq '.runtime' app.yaml | sed -e 's/go\([0-9]\)\([0-9]*\)/\1.\2/g')
 go mod tidy -go="${go_version}"
