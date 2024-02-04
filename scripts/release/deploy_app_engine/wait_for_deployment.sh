3da38d8bd8575b8ec2181077a49bb3840df8f3b5
massongit <suzukimasaya428@gmail.com>
2024-02-04 19:05:31 +0900

    Fix format

--- scripts/release/deploy_app_engine/wait_for_deployment.sh
+++ scripts/release/deploy_app_engine/wait_for_deployment.sh
@@ -2,18 +2,18 @@
 
 # 最大10分待つ
 for i in $(seq 600); do
-  serving_status=$(gcloud app versions describe \
-    "v${RUN_NUMBER}" \
-    --service "default" \
-    --format \
-    "value(servingStatus)")
-  echo "${i}: servingStatus: ${serving_status}"
+	serving_status=$(gcloud app versions describe \
+		"v${RUN_NUMBER}" \
+		--service "default" \
+		--format \
+		"value(servingStatus)")
+	echo "${i}: servingStatus: ${serving_status}"
 
-  if [ "${serving_status}" = "SERVING" ]; then
-    exit 0
-  fi
+	if [ "${serving_status}" = "SERVING" ]; then
+		exit 0
+	fi
 
-  sleep 1
+	sleep 1
 done
 
 exit 1
