3e8b6d5ec751467bc46f96cb48042837766d17ed
Masaya Suzuki <15100604+massongit@users.noreply.github.com>
2024-02-06 07:17:04 +0900

    super-linterアップデート (#3955)
    * chore(deps): update super-linter/super-linter action to v6
    
    * Add fetch-depth: 0
    
    * empty commit
    
    * Add VALIDATE_GO: false
    
    * Fix format
    
    * Add comment
    
    * chmod +x
    
    * Fix format
    
    * Add user
    
    * Fix
    
    * Set permission
    
    * Dockleをアップデートしてあげたよ！ (#3957)
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Revert
    
    * skip CKV_DOCKER_3
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- gcp/datastore/start.sh
+++ gcp/datastore/start.sh
@@ -3,6 +3,6 @@
 gcloud config set project "${DATASTORE_PROJECT_ID}"
 
 gcloud beta emulators datastore start \
-  --data-dir=/opt/data \
-  --host-port="${DATASTORE_LISTEN_ADDRESS}" \
-  "${@}"
+	--data-dir=/opt/data \
+	--host-port="${DATASTORE_LISTEN_ADDRESS}" \
+	"${@}"
