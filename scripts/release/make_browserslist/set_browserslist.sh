8fb6428d6dda608be9db084e8253bc0aa351170b
Masaya Suzuki <15100604+massongit@users.noreply.github.com>
2024-06-18 10:33:35 +0900

    CI make-browserslist: 具体的なバージョンをmozillaから取得する (#4294)
    * CI make-browserslist: 具体的なバージョンをmozillaから取得する
    
    * fix browserslist
    
    * ESRを考慮する
    
    * beta版除く
    
    * fix format
    
    * add double quote

--- scripts/release/make_browserslist/set_browserslist.sh
+++ scripts/release/make_browserslist/set_browserslist.sh
@@ -1,8 +1,22 @@
 #!/usr/bin/env bash
 
 npm ci
-browsers="$(npx browserslist | grep firefox | sed -e 's/$/.0.1/')"
-# shellcheck disable=SC2001
-browserslist="[$(echo "${browsers}" | sed -e 's/^\([^ ]*\) \(.*\)/{browser_name: "\1", browser_version: "\2"}/' | tr '\n' ',' | sed -e 's/,$//')]"
+raw_versions="$(curl https://product-details.mozilla.org/1.0/firefox.json)"
+versions="$(echo "$raw_versions" | yq -r '.releases.[] | .date + " " + .version + " " + .category' | sort -r)"
+browsers=""
+
+for version in $(npx browserslist | grep firefox | awk '{print $2}'); do
+	version_data="$(echo "$versions" | grep " $version.[^b]* " | head -n 1)"
+	browser_version="$(echo "$version_data" | awk '{print $2}')"
+	category="$(echo "$version_data" | awk '{print $3}')"
+
+	if [ "$category" = 'esr' ]; then
+		browser_version+="$category"
+	fi
+
+	browsers+="{browser_name: \"firefox\", browser_version: \"$browser_version\"}"
+done
+
+browserslist="[${browsers//\}\{/\},{}]"
 echo "${browserslist}"
 echo "browserslist=${browserslist}" >>"${GITHUB_OUTPUT}"
