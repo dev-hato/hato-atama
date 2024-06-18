0dc8640908f98451a3d7ede6bfbe529d29c08ffd
Masaya Suzuki <suzukimasaya428@gmail.com>
2024-06-18 09:23:37 +0900

    CI make-browserslist: 具体的なバージョンをmozillaから取得する

--- scripts/release/make_browserslist/set_browserslist.sh
+++ scripts/release/make_browserslist/set_browserslist.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
 npm ci
-browsers="$(npx browserslist | grep firefox | sed -e 's/$/.0.1/')"
-# shellcheck disable=SC2001
-browserslist="[$(echo "${browsers}" | sed -e 's/^\([^ ]*\) \(.*\)/{browser_name: "\1", browser_version: "\2"}/' | tr '\n' ',' | sed -e 's/,$//')]"
+raw_versions="$(curl https://product-details.mozilla.org/1.0/firefox.json)"
+versions="$(echo "$raw_versions" | yq -r '.releases.[] | .date + " " + .version' | sort -r)"
+browsers=""
+
+for version in $(npx browserslist | grep firefox | awk '{print $2}')
+do
+  browser_version="$(echo "$versions" | grep " $version.[^b]*\$" | head -n 1 | awk '{print $2}')"
+  browsers+="{browser_name: \"firefox\", browser_version: \"$browser_version\"}"
+done
+
+browserslist="${browsers//\}\{/\},{}"
 echo "${browserslist}"
 echo "browserslist=${browserslist}" >>"${GITHUB_OUTPUT}"
