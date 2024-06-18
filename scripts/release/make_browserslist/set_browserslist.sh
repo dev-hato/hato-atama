492cd685d8fbdbcf02ad6707b2f56fd51f3f552d
Masaya Suzuki <suzukimasaya428@gmail.com>
2024-06-18 10:19:42 +0900

    fix format

--- scripts/release/make_browserslist/set_browserslist.sh
+++ scripts/release/make_browserslist/set_browserslist.sh
@@ -5,18 +5,16 @@ raw_versions="$(curl https://product-details.mozilla.org/1.0/firefox.json)"
 versions="$(echo "$raw_versions" | yq -r '.releases.[] | .date + " " + .version + " " + .category' | sort -r)"
 browsers=""
 
-for version in $(npx browserslist | grep firefox | awk '{print $2}')
-do
-  version_data="$(echo "$versions" | grep " $version.[^b]* " | head -n 1)"
-  browser_version="$(echo "$version_data" | awk '{print $2}')"
-  category="$(echo "$version_data" | awk '{print $3}')"
+for version in $(npx browserslist | grep firefox | awk '{print $2}'); do
+	version_data="$(echo "$versions" | grep " $version.[^b]* " | head -n 1)"
+	browser_version="$(echo "$version_data" | awk '{print $2}')"
+	category="$(echo "$version_data" | awk '{print $3}')"
 
-  if [ "$category" = 'esr' ]
-  then
-    browser_version+=$category
-  fi
+	if [ "$category" = 'esr' ]; then
+		browser_version+=$category
+	fi
 
-  browsers+="{browser_name: \"firefox\", browser_version: \"$browser_version\"}"
+	browsers+="{browser_name: \"firefox\", browser_version: \"$browser_version\"}"
 done
 
 browserslist="[${browsers//\}\{/\},{}]"
