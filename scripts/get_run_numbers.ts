fdc9525b5f44dd7be22b04edce39fda81b7cb037
Masaya Suzuki <suzukimasaya428@gmail.com>
2025-01-28 21:09:46 +0900

    test

--- scripts/get_run_numbers.ts
+++ scripts/get_run_numbers.ts
@@ -65,7 +65,6 @@ export async function script(
           .filter((r) => r.event === "push" || r.status === "completed")
           .map((r): string => {
             if (r.status !== "completed") {
-              console.log(r.event,r.status,r.run_number);
               return running;
             }
 
@@ -79,6 +78,7 @@ export async function script(
       result.shift();
     }
 
+    console.log(result.includes(running));
     await sleep(result, running, retryCount, i);
   }
 
