b80ea65d1cd79cf9f12b1087e77f77313bb8f428
masaya.suzuki <suzukimasaya428@gmail.com>
2025-12-19 19:40:17 +0900

    middleware.Logger -> middleware.RequestLogger

--- server/main.go
+++ server/main.go
@@ -83,7 +83,7 @@ func createServer() (e *echo.Echo) {
 	e = echo.New()
 
 	e.Use(middleware.Recover())
-	e.Use(middleware.Logger())
+	e.Use(middleware.RequestLogger())
 	e.Use(middleware.Gzip())
 
 	api := e.Group("/api")
