7e392966f62906d3759ded6fccbf8420eb21265d
masaya.suzuki <suzukimasaya428@gmail.com>
2026-01-28 21:21:50 +0900

    shfmt  -l -w .

--- scripts/run_go_mod_tidy.sh
+++ scripts/run_go_mod_tidy.sh
@@ -4,7 +4,7 @@ set -e
 new_go_version=$(yq '.runtime' app.yaml | sed -e 's/go\([0-9]\)\([0-9]*\)/\1.\2/g')
 
 if echo "$GO_VERSION" | grep "^$new_go_version"; then
-  new_go_version=$GO_VERSION
+	new_go_version=$GO_VERSION
 fi
 
 go mod tidy -go="${new_go_version}"
