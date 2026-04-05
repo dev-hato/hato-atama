4cf9f781bdc705c4d8164f5d48e0b283f4e5542a
Masaya Suzuki <15100604+massongit@users.noreply.github.com>
2026-04-06 06:33:54 +0900

    Revert "Aikido Safe Chain追加 (#5542)" (#5852)
    This reverts commit e90316fa0191af11acfc1b6bfbf629ed418ac6b0.

--- scripts/release/build_frontend/build.sh
+++ scripts/release/build_frontend/build.sh
@@ -7,6 +7,5 @@ curl -L -o elm.gz "https://github.com/elm/compiler/releases/download/${elm_versi
 gunzip elm.gz
 chmod +x elm
 mv elm /usr/local/bin/
-curl -fsSL https://raw.githubusercontent.com/AikidoSec/safe-chain/main/install-scripts/install-safe-chain.sh | sh -s -- --ci
 npm ci
 npm run build
