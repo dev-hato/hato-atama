6445493e6045a58ee9b6f37aeb643bc11f17f208
masaya.suzuki <suzukimasaya428@gmail.com>
2026-04-05 10:06:16 +0900

    Revert "Aikido Safe Chain追加 (#5542)"
    This reverts commit e90316fa0191af11acfc1b6bfbf629ed418ac6b0.

--- scripts/resource_update/update.sh
+++ scripts/resource_update/update.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 set -e
 
-curl -fsSL https://raw.githubusercontent.com/AikidoSec/safe-chain/main/install-scripts/install-safe-chain.sh | sh -s -- --ci
 npx npm-check-updates -u
 npm install
