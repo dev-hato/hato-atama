e873befd8bab4dc547fee8e067d8f47143fa3dc5
masaya.suzuki <suzukimasaya428@gmail.com>
2026-05-16 20:56:03 +0900

    environment追加

--- scripts/release/make_browserslist/set_browserslist.sh
+++ scripts/release/make_browserslist/set_browserslist.sh
@@ -6,9 +6,11 @@ raw_versions="$(curl https://product-details.mozilla.org/1.0/firefox.json)"
 versions="$(echo "$raw_versions" | yq -r '.releases.[] | .date + " " + key' | sort -r)"
 browsers=""
 
-for version in $(npx browserslist | grep firefox | awk '{print $2}'); do
-	browser_version="$(echo "$versions" | grep "$version.[^b]*" | head -n 1 | awk '{print $2}' | sed -e 's/firefox-//g')"
-	browsers+="{browser_name: \"firefox\", browser_version: \"$browser_version\"}"
+for environment in staging dev; do
+  for version in $(npx browserslist | grep firefox | awk '{print $2}'); do
+    browser_version="$(echo "$versions" | grep "$version.[^b]*" | head -n 1 | awk '{print $2}' | sed -e 's/firefox-//g')"
+    browsers+="{browser_name: \"firefox\", browser_version: \"$browser_version\", environment: \"$environment\"}"
+  done
 done
 
 browserslist="[${browsers//\}\{/\},{}]"
