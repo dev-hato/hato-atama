c8a29bf2e3ed847bc62028d62b633d1d9780dd91
masaya.suzuki <suzukimasaya428@gmail.com>
2026-05-16 20:49:39 +0900

    ファイル名修正

--- scripts/release/run_docker_compose_dev.sh
+++ scripts/release/run_docker_compose_dev.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
 
-docker compose -f compose.yml -f staging.compose.yml pull
-docker compose -f compose.yml -f staging.compose.yml up -d --wait
+docker compose -f compose.yml -f dev.base.compose.yml pull
+docker compose -f compose.yml -f dev.base.compose.yml up -d --wait
